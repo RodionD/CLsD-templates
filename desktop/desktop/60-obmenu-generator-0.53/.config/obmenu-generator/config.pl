7f82f355bc1798e3ff743edb3c895afa1d806a8f
RodionD <rodion2000@gmail.com>
2017-08-28 15:40:02 +0300

    alpha 2

--- desktop/desktop/60-obmenu-generator-0.53/.config/obmenu-generator/config.pl
+++ desktop/desktop/60-obmenu-generator-0.53/.config/obmenu-generator/config.pl
@@ -1,37 +1,98 @@
 #!/usr/bin/perl
 
-# desktop_files_paths        : Example: [ "$ENV{'HOME'}/.local/share/applications", '/my/path' ]
-# gtk_rc_file                : File where to look for the icon theme (default: ~/.gtkrc-2.0)
-# skip_file_name_re          : Ignore desktop files if their filenames match a regex
-# skip_app_name_re           : Ignore applications if their names match a regex
-# skip_app_command_re        : Ignore applications if their commands match a regex
-# skip_file_content_re       : Ignore desktop files if their content match a regex
-# subst_command_name_re      : Remove from every command something matched by a regex (/g)
-# icon_dirs_first            : Look in this directories before icon theme (when generating icons.db)
-# icon_dirs_second           : Look in this directories after the icon theme (when generating icons.db)
-# icon_dirs_last             : Look in this directories after all directories (when generating icons.db)
+# obmenu-generator - configuration file
+# This file will be updated automatically.
+# Any additional comment and/or indentation will be lost.
 
-# For regular expressions
-#    * is better to use qr/REGEX/ instead of 'REGEX'
+=for comment
 
-# NOTE: Once an icon is found, it will *NOT* be replaced by another.
+|| FILTERING
+    | skip_filename_re    : Skip a .desktop file if its name matches the regex.
+                            Name is from the last slash to the end. (filename.desktop)
+                            Example: qr/^(?:gimp|xterm)\b/,    # skips 'gimp' and 'xterm'
+
+    | skip_entry          : Skip a destkop file if the value from a given key matches the regex.
+                            Example: [
+                                {key => 'Name', re => qr/(?:about|terminal)/i},
+                                {key => 'Exec', re => qr/^xterm/},
+                            ],
+
+    | substitutions       : Substitute, by using a regex, in the values of the desktop files.
+                            Example: [
+                                {key => 'Exec', re => qr/xterm/, value => 'sakura'},
+                                {key => 'Exec', re => qr/\\\\/,  value => '\\', global => 1},    # for wine apps
+                            ],
+
+
+|| ICON SETTINGS
+    | icon_dirs_first     : When looking for icons, look in this directories first,
+                            before looking in the directories of the current icon theme.
+                            Example: [
+                                "$ENV{HOME}/My icons",
+                            ],
+
+    | icon_dirs_second    : Look in this directories after looked in the directories of the
+                            current icon theme. (Before /usr/share/pixmaps)
+                            Example: [
+                                "/usr/share/icons/gnome",
+                            ],
+
+    | icon_dirs_last      : Look in this directories at the very last, after looked in
+                            /usr/share/pixmaps, /usr/share/icons/hicolor and some other
+                            directories.
+                            Example: [
+                                "/usr/share/icons/Tango",
+                            ],
+
+    | strict_icon_dirs    : A true value will make the script to look only inside the directories
+                            specified by you in either one of the above three options.
+
+    | gtk_rc_filename     : Absolute path to the GTK configuration file.
+    | missing_image       : Use this icon for missing icons (default: gtk-missing-image)
+
+
+|| KEYS
+    | name_keys           : Valid keys for the item names.
+                            Example: ['Name[fr]', 'GenericName[fr]', 'Name'],   # french menu
+
+
+|| PATHS
+    | desktop_files_paths   : Absolute paths which contain .desktop files.
+                              Example: [
+                                '/usr/share/applications',
+                                "$ENV{HOME}/.local/share/applications",
+                                glob("$ENV{HOME}/.local/share/applications/wine/Programs/*"),
+                              ],
+
+
+|| NOTES
+    | Regular expressions:
+        * use qr/RE/ instead of 'RE'
+        * use qr/RE/i for case insensitive mode
+
+=cut
 
 our $CONFIG = {
-  categories_case_sensitive => 0,
-  clean_command_name_re     => undef,
-  desktop_files_paths       => ["/usr/share/applications", "$ENV{'HOME'}/.local/share/applications"],
-  editor                    => "/usr/bin/nano",
-  gtk_rc_filename           => "~/gtkrc-2.0",
-  icon_dirs_first           => [],
-  icon_dirs_last            => ["/usr/share/icons/gnome"],
-  icon_dirs_second          => [],
-  skip_app_command_re       => undef,
-  skip_app_name_re          => undef,
-  skip_file_content_re      => undef,
-  skip_file_name_re         => undef,
-  skip_svg_icons            => 0,
-  terminal                  => "urxvt",
-  web_browser               => "firefox-bin",
-  use_only_my_icon_dirs     => 0,
-  VERSION                   => 0.53,
+  "editor"              => "leafpad",
+  "Linux::DesktopFiles" => {
+                             desktop_files_paths     => ["/usr/share/applications", "/usr/local/share/applications"],
+                             gtk_rc_filename         => "~/.gtkrc-2.0",
+                             icon_dirs_first         => undef,
+                             icon_dirs_last          => undef,
+                             icon_dirs_second        => undef,
+                             keep_unknown_categories => 1,
+                             skip_entry              => undef,
+                             skip_filename_re        => undef,
+                             skip_svg_icons          => 0,
+                             strict_icon_dirs        => undef,
+                             substitutions           => undef,
+                             terminalization_format  => "%s -e '%s'",
+                             terminalize             => 1,
+                             unknown_category_key    => "other",
+                           },
+  "missing_icon"        => "gtk-missing-image",
+  "name_keys"           => ["Name"],
+  "terminal"            => "urxvt",
+  "web_browser"         => "firefox",
+  "VERSION"             => 0.66,
 }
--- desktop/desktop/60-obmenu-generator-0.53/.config/obmenu-generator/schema.pl
+++ desktop/desktop/60-obmenu-generator-0.53/.config/obmenu-generator/schema.pl
@@ -1,36 +1,41 @@
-#Calculate
 #!/usr/bin/perl
 
-# SCHEMA supports the following keys: item, cat, begin_cat, end_cat,
-#                                     exit, raw, sep, obgenmenu
-#
-# Posible values for each of this types are:
-# For 'item' : [COMMAND, LABEL, ICON] - icon is optional
-# For 'sep'  : A string representing the LABEL for the separator or undef for none
-# For 'cat'  : Any of the possible categories. 'cat => [CATEGORY, LABEL, ICON]'
-# For 'exit' : undef - default "Exit" action
-# For 'raw'  : A hardcoded XML line in the Openbox's menu.xml file format
-#    Example : {raw => '<menu icon="" id="client-list-combined-menu" />'},
+# obmenu-generator - schema file
+
+=for comment
+
+    item:      add an item inside the menu               {item => ["command", "label", "icon"]},
+    cat:       add a category inside the menu             {cat => ["name", "label", "icon"]},
+    sep:       horizontal line separator                  {sep => undef}, {sep => "label"},
+    pipe:      a pipe menu entry                         {pipe => ["command", "label", "icon"]},
+    raw:       any valid Openbox XML string               {raw => q(xml string)},
+    begin_cat: begin of a category                  {begin_cat => ["name", "icon"]},
+    end_cat:   end of a category                      {end_cat => undef},
+    obgenmenu: generic menu settings                {obgenmenu => ["label", "icon"]},
+    exit:      default "Exit" action                     {exit => ["label", "icon"]},
+
+=cut
 
 # NOTE:
 #    * Keys and values are case sensitive. Keep all keys lowercase.
-#    * ICON can be a either a direct path to a icon or a valid icon name
-#    * Category names are case insensitive. (ex: X-XFCE and x_xfce are equivalent)
+#    * ICON can be a either a direct path to an icon or a valid icon name
+#    * Category names are case insensitive. (X-XFCE and x_xfce are equivalent)
 
-require '/etc/obmenu-generator/config.pl';
+require "$ENV{HOME}/.config/obmenu-generator/config.pl";
+
+## Text editor
+my $editor = $CONFIG->{editor};
 
 #?os_locale_language!=ru#
 our $SCHEMA = [
-    #             COMMAND                 LABEL                ICON
-    {item => ['spacefm',             'File Manager',      'file-manager']},
-    {item => [$CONFIG->{terminal},   'Terminal',          'terminal']},
-    {item => [$CONFIG->{editor},     'Editor',            'text-editor']},
-    {item => [$CONFIG->{web_browser},'Web Browser',       'web-browser']},
-    {item => ['gmrun',               'Run command',       'system-run']},
-    {item => ['pidgin',              'Instant messaging', 'system-users']},
-    {raw => '<menu execute="obdevicemenu" icon="/usr/share/icons/gnome/32x32/devices/harddrive.png" id="mounts" label="Removable Devices"/>'},
 
-    {sep => 'Applications'},
+    #          COMMAND                 LABEL                ICON
+    {item => ['xdg-open .',        'File Manager',      'file-manager']},
+    {item => [$CONFIG->{terminal},             'Terminal',          'terminal']},
+    {item => [$CONFIG->{web_browser},'Web Browser',       'web-browser']},
+    {item => ['gmrun',             'Run command',       'system-run']},
+
+    {sep => 'Categories'},
 
     #          NAME            LABEL                ICON
     {cat => ['utility',     'Accessories', 'applications-utilities']},
@@ -41,44 +46,85 @@ our $SCHEMA = [
     {cat => ['audiovideo',  'Multimedia',  'applications-multimedia']},
     {cat => ['network',     'Network',     'applications-internet']},
     {cat => ['office',      'Office',      'applications-office']},
+    {cat => ['other',       'Other',       'applications-other']},
     {cat => ['settings',    'Settings',    'applications-accessories']},
     {cat => ['system',      'System',      'applications-system']},
+
+    #{cat => ['qt',          'QT Applications',    'qt4logo']},
+    #{cat => ['gtk',         'GTK Applications',   'gnome-applications']},
+    #{cat => ['x_xfce',      'XFCE Applications',  'applications-other']},
+    #{cat => ['gnome',       'GNOME Applications', 'gnome-applications']},
+    #{cat => ['consoleonly', 'CLI Applications',   'applications-utilities']},
 
     #                  LABEL          ICON
     #{begin_cat => ['My category',  'cat-icon']},
     #             ... some items ...
     #{end_cat   => undef},
 
-    #{cat => ['qt',          'QT Applications',    'qtlogo']},
-    #{cat => ['gtk',         'GTK Applications',   'gnome-applications']},
-    #{cat => ['x_xfce',      'XFCE Applications',  'applications-other']},
-    #{cat => ['gnome',       'GNOME Applications', 'gnome-applications']},
-    #{cat => ['consoleonly', 'CLI Applications',   'applications-utilities']},
+    #            COMMAND     LABEL        ICON
+    #{pipe => ['obbrowser', 'Disk', 'drive-harddisk']},
 
-    {obgenmenu => 'Openbox Settings'},
-    {sep       => undef},
+    ## Generic advanced settings
+    #{sep       => undef},
+    #{obgenmenu => ['Openbox Settings', 'openbox']},
+    #{sep       => undef},
 
+    ## Custom advanced settings
+    {sep => undef},
+    {begin_cat => ['Advanced Settings', 'gnome-settings']},
+
+        # Configuration files
+        {item => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', 'text-x-source']},
+
+        # obmenu-generator category
+        {begin_cat => ['Obmenu-Generator', 'menu-editor']},
+            {item => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', 'text-x-source']},
+            {item => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', 'text-x-source']},
+
+            {sep  => undef},
+            {item => ['obmenu-generator -p',       'Generate a pipe menu',              'menu-editor']},
+            {item => ['obmenu-generator -s -c',    'Generate a static menu',            'menu-editor']},
+            {item => ['obmenu-generator -p -i',    'Generate a pipe menu with icons',   'menu-editor']},
+            {item => ['obmenu-generator -s -i -c', 'Generate a static menu with icons', 'menu-editor']},
+            {sep  => undef},
+
+            {item => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
+        {end_cat => undef},
+
+        # Openbox category
+        {begin_cat => ['Openbox', 'openbox']},
+            {item => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
+            {item => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',   'shellscript']},
+            {item => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-xml']},
+            {item => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',        'text-xml']},
+        {end_cat => undef},
+
+    {end_cat => undef},
+    {sep => undef},
+
+    ## The xscreensaver lock command
     {item => ['xscreensaver-command -lock', 'Lock', 'lock']},
 
-    # This options uses the default OpenBox action "Exit"
-    {item => ['obshutdown', 'Exit']},
+    # This option uses the default Openbox's action "Exit"
+    {exit => ['Exit', 'exit']},
+
+    # This uses the 'oblogout' menu
+    # {item => ['oblogout', 'Exit', 'exit']},
 ]
 #os_locale_language#
 #?os_locale_language=ru#
 our $SCHEMA = [
-    #             COMMAND                 LABEL                ICON
-    {item => ['spacefm',             'Менеджер файлов',      'file-manager']},
-    {item => [$CONFIG->{terminal},   'Терминал',          'terminal']},
-    {item => [$CONFIG->{editor},     'Редактор',            'text-editor']},
-    {item => [$CONFIG->{web_browser},'Броузер',       'web-browser']},
-    {item => ['gmrun',               'Выполнить команду',       'system-run']},
-    {item => ['pidgin',              'Мгновенные сообщения', 'system-users']},
-    {raw => '<menu execute="obdevicemenu" icon="/usr/share/icons/gnome/32x32/devices/harddrive.png" id="mounts" label="Внешние накопители"/>'},
 
-    {sep => 'Applications'},
+    #          COMMAND                 LABEL                ICON
+    {item => ['xdg-open .',        'Файловый менеджер',      'file-manager']},
+    {item => [$CONFIG->{terminal},             'Терминал',          'terminal']},
+    {item => [$CONFIG->{web_browser},'Web браузер',       'web-browser']},
+    {item => ['gmrun',             'Выполнить комманду',       'system-run']},
+
+    {sep => 'Categories'},
 
     #          NAME            LABEL                ICON
-    {cat => ['utility',     'Утилиты', 'applications-utilities']},
+    {cat => ['utility',     'Служебные', 'applications-utilities']},
     {cat => ['development', 'Разработка', 'applications-development']},
     {cat => ['education',   'Образование',   'applications-science']},
     {cat => ['game',        'Игры',       'applications-games']},
@@ -86,26 +132,68 @@ our $SCHEMA = [
     {cat => ['audiovideo',  'Мультимедия',  'applications-multimedia']},
     {cat => ['network',     'Сеть',     'applications-internet']},
     {cat => ['office',      'Офис',      'applications-office']},
+    {cat => ['other',       'прочее',       'applications-other']},
     {cat => ['settings',    'Настройки',    'applications-accessories']},
     {cat => ['system',      'Система',      'applications-system']},
+
+    #{cat => ['qt',          'QT Applications',    'qt4logo']},
+    #{cat => ['gtk',         'GTK Applications',   'gnome-applications']},
+    #{cat => ['x_xfce',      'XFCE Applications',  'applications-other']},
+    #{cat => ['gnome',       'GNOME Applications', 'gnome-applications']},
+    #{cat => ['consoleonly', 'CLI Applications',   'applications-utilities']},
 
     #                  LABEL          ICON
     #{begin_cat => ['My category',  'cat-icon']},
     #             ... some items ...
     #{end_cat   => undef},
 
-    #{cat => ['qt',          'QT Applications',    'qtlogo']},
-    #{cat => ['gtk',         'GTK Applications',   'gnome-applications']},
-    #{cat => ['x_xfce',      'XFCE Applications',  'applications-other']},
-    #{cat => ['gnome',       'GNOME Applications', 'gnome-applications']},
-    #{cat => ['consoleonly', 'CLI Applications',   'applications-utilities']},
+    #            COMMAND     LABEL        ICON
+    #{pipe => ['obbrowser', 'Disk', 'drive-harddisk']},
 
-    {obgenmenu => 'Настройки Openbox'},
-    {sep       => undef},
+    ## Generic advanced settings
+    #{sep       => undef},
+    #{obgenmenu => ['Openbox Settings', 'openbox']},
+    #{sep       => undef},
 
-    {item => ['xscreensaver-command -lock', 'Блокировка экрана', 'lock']},
+    ## Custom advanced settings
+    {sep => undef},
+    {begin_cat => ['Расширенные настройки', 'gnome-settings']},
 
-    # This options uses the default OpenBox action "Exit"
-    {item => ['obshutdown', 'Выход']},
-]
+        # Configuration files
+        {item => ["$editor ~/.config/tint2/tint2rc", 'Панель Tint2', 'text-x-source']},
+
+        # obmenu-generator category
+        {begin_cat => ['Obmenu-Generator', 'menu-editor']},
+            {item => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', 'text-x-source']},
+            {item => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', 'text-x-source']},
+
+            {sep  => undef},
+            {item => ['obmenu-generator -p',       'Генерация динамического меню',              'menu-editor']},
+            {item => ['obmenu-generator -s -c',    'Генерация статического меню',            'menu-editor']},
+            {item => ['obmenu-generator -p -i',    'Генерация динамического меню с иконками',   'menu-editor']},
+            {item => ['obmenu-generator -s -i -c', 'Генерация статического меню с иконками', 'menu-editor']},
+            {sep  => undef},
+
+            {item => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
+        {end_cat => undef},
+
+        # Openbox category
+        {begin_cat => ['Openbox', 'openbox']},
+            {item => ['openbox --reconfigure',               'Реконфигурация Openbox', 'openbox']},
+            {item => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',   'shellscript']},
+            {item => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-xml']},
+            {item => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',        'text-xml']},
+        {end_cat => undef},
+
+    {end_cat => undef},
+    {sep => undef},
+
+    ## The xscreensaver lock command
+    {item => ['xscreensaver-command -lock', 'Блокировка', 'lock']},
+
+    # This option uses the default Openbox's action "Exit"
+    {exit => ['Выход', 'exit']},
+
+    # This uses the 'oblogout' menu
+    # {item => ['oblogout', 'Exit', 'exit']},
 #os_locale_language#
